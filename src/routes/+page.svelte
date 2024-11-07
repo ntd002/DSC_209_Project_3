c945edd26cef2390724dcd0709df715b3470eeea
ntd002 <ntd002@ucsd.edu>
2024-11-06 19:52:03 -0800

    Full Project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,30 +20,36 @@
         for (let i = 0; i < poke.results.length; i++) {
             let tempRes = await fetch(poke.results[i].url)
             let tempPoke = await tempRes.json()
-            
-                //handle if they have a second type
-            if (tempPoke.types.length === 1) {
-                data.push({
-                name: poke.results[i].name.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '),
-                bst: tempPoke.stats[0].base_stat + tempPoke.stats[1].base_stat + tempPoke.stats[2].base_stat + tempPoke.stats[3].base_stat + tempPoke.stats[4].base_stat + tempPoke.stats[5].base_stat, 
-                type1: tempPoke.types[0].type.name,
-                type2: 'none',
-                order: tempPoke.order,
-                weightG: tempPoke.weight,
-                sprite: tempPoke.sprites.front_default,
-                })
+
+                //ignore unordered pokemon
+            if (tempPoke.order === -1) {
+                //console.log(poke.results[i].name)
             }
             else {
-                data.push({
-                name: poke.results[i].name.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '),
-                bst: tempPoke.stats[0].base_stat + tempPoke.stats[1].base_stat + tempPoke.stats[2].base_stat + tempPoke.stats[3].base_stat + tempPoke.stats[4].base_stat + tempPoke.stats[5].base_stat, 
-                type1: tempPoke.types[0].type.name,
-                type2: tempPoke.types[1].type.name,
-                order: tempPoke.order,
-                weightG: tempPoke.weight,
-                sprite: tempPoke.sprites.front_default,
-                })
+                //handle if they have a second type
+                if (tempPoke.types.length === 1) {
+                    data.push({
+                    name: poke.results[i].name,
+                    bst: tempPoke.stats[0].base_stat + tempPoke.stats[1].base_stat + tempPoke.stats[2].base_stat + tempPoke.stats[3].base_stat + tempPoke.stats[4].base_stat + tempPoke.stats[5].base_stat, 
+                    type1: tempPoke.types[0].type.name,
+                    type2: 'none',
+                    order: tempPoke.order,
+                    sprite: tempPoke.sprites.front_default,
+                    })
+                }
+                else {
+                    data.push({
+                    name: poke.results[i].name,
+                    bst: tempPoke.stats[0].base_stat + tempPoke.stats[1].base_stat + tempPoke.stats[2].base_stat + tempPoke.stats[3].base_stat + tempPoke.stats[4].base_stat + tempPoke.stats[5].base_stat, 
+                    type1: tempPoke.types[0].type.name,
+                    type2: tempPoke.types[1].type.name,
+                    order: tempPoke.order,
+                    sprite: tempPoke.sprites.front_default,
+                    })
+                }
             }
+            
+                
         }
         
         data = data;
@@ -52,7 +58,7 @@
 </script>
 
 <main>
-    <h1>Pokémon Base Stats and Weight Trends</h1>
+    <h1>Pokémon Base Stats and National Dex Trend</h1>
     <PokeAPI {data}/>
 </main>
 
--- src/routes/PokeAPI.svelte
+++ src/routes/PokeAPI.svelte
@@ -1,16 +1,26 @@
 <script>
     import * as d3 from 'd3';
     export let data;
-    const width = 923;
+    const width = 1000;
     const height = 600;
     const marginTop = 20;
-    const marginBottom = 30;
-    const marginRight = 30;
-    const marginLeft = 40;
+    const marginBottom = 100;
+    const marginRight = 150;
+    const marginLeft = 100;
+
+    let svg;
+
+    //for axis
+    let gx;
+    let gy;
+
+    let dataFilter = data;
+    let circles;
+    let selectButt;
 
     $: x = d3  
         .scaleLinear()
-        .domain(d3.extent(data, (d) => d.weightG))
+        .domain(d3.extent(data, (d) => d.order))
         .range([marginLeft, width - marginRight]);
     $: y = d3  
         .scaleLinear()
@@ -18,21 +28,166 @@
         .nice()
         .range([height - marginBottom, marginTop]);
 
-    $: console.log(data.length);
+    //lowest pokemon bst is 175, highest is 1125
+    $: color = d3
+        .scaleSequential()
+        .domain([150, 1200])
+        .interpolator(d3.interpolateRdYlBu)
+    
+    //axis
+    $: d3.select(gx).call(d3.axisBottom(x));
+    $: d3.select(gy).call(d3.axisLeft(y));
+
+    //tooltip
+    const bisect = d3.bisector((d) => d.order).center;
+    let tooltipPt = null;
+    //const quadtree = d3.quadtree(data, d => d.bst, d => d.order);
+    function onPointerMove(event) {
+        const [xValue, yValue] = d3.pointer(event);
+        const i = bisect(dataFilter, x.invert(xValue))
+        tooltipPt = dataFilter[i];
+
+        //d3.select(circles).selectAll("*").remove();
+        //const closestPoint = quadtree.find(xValue, yValue);
+        //console.log(closestPoint)
+        //tooltipPt = quadtree.find(yValue, xValue);
+    }
+
+    $: d3.select(svg)
+    .on('pointerenter pointermove', onPointerMove)
+
+    //filtering
+    function update(selectedType) {
+        dataFilter = []
+        // Create new data with the selection
+        if (selectedType === "any") {
+            dataFilter = data
+        }
+        else {
+            for (let i = 0; i < data.length; i++) {
+                //handle if they contain the type
+                if (containsType(data[i], selectedType)){
+                    dataFilter.push({
+                        name: data[i].name,
+                        bst: data[i].bst,
+                        type1: data[i].type1,
+                        type2: data[i].type2,
+                        order: data[i].order,
+                        sprite: data[i].sprite,
+                    })
+                }
+            }
+        }
+        
+        //delete all circles
+        d3.select(circles).selectAll("*").remove();
+
+        //replace them
+        for (let i = 0; i < dataFilter.length; i++) {
+            d3.select(circles).append("circle")
+                .attr("key", dataFilter[i].order)
+                .attr("cx", x(dataFilter[i].order))
+                .attr("cy", y(dataFilter[i].bst))
+                .attr("fill", color(dataFilter[i].bst))
+                .attr("r", 2.5)
+        }
+    }
+
+    function containsType(d,type) {
+        if (d.type1 === type) {
+            return true;
+        }
+        else if (d.type2 === type) {
+            return true;
+        }
+        return false
+    }
+
+    var allTypes = ["any", "normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy"]
+
+    $: d3.select(selectButt)
+      .selectAll('myOptions')
+     	.data(allTypes)
+      .enter()
+    	.append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; })
+
+    $: d3.select(selectButt).on("change", function(d) {
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    })
+
 </script>
+
+<select bind:this={selectButt}></select>
 
 <div class="pokemon-plot">
     <svg
+    bind:this={svg}
     {width}
     {height}
     viewBox = "0 0 {width} {height}"
     style="max-width: 100%; height:auto;"
     >
-    <g stroke="#000" stroke-opacity="0.2">
-        {#each data as d, i}
-            <circle key={i} cx={x(d.weightG)} cy={y(d.bst)} fill={'blue'} r="2.5"/>
+    <g bind:this={gx} transform="translate(0, {height - marginBottom})"></g>
+    <g bind:this={gy} transform="translate({marginLeft},0)"></g>
+
+    <text class="axis-Title" text-anchor="end" x={width-marginRight} y={height-55}>Base Stat Total</text>
+    <text class="axis-Title" text-anchor="end" transform="rotate(-90)" x={marginTop-40} y={marginLeft-40}>Order</text>
+
+    <g bind:this={circles} stroke="#000" stroke-opacity="0.2">
+        {#each data as d}
+            <circle 
+                key={d.order} 
+                cx={x(d.order)} 
+                cy={y(d.bst)} 
+                fill={color(d.bst)} 
+                r="2.5"/>
         {/each}
     </g>
-
+    {#if tooltipPt}
+        <g transform="translate({x(tooltipPt.order)},{y(tooltipPt.bst)})">
+            <text font-weight="bold">
+                {tooltipPt.name}
+            </text>
+        </g>
+        <g transform="translate(200,100)">
+            <text font-weight="bold">
+                {tooltipPt.name}
+            </text>
+            <image xlink:href="{tooltipPt.sprite}" alt="{tooltipPt.name}">
+        </g>
+        <g transform="translate(300,120)">
+            <text font-weight="bold">
+                {#if tooltipPt.type2==="none"}
+                    Type: {tooltipPt.type1}
+                {:else}
+                    Type: {tooltipPt.type1} {tooltipPt.type2}
+                {/if}
+            </text>
+        </g>
+        <g transform="translate(300,140)">
+            <text font-weight="bold">
+                Base Stat Total: {tooltipPt.bst}
+            </text>
+        </g>
+        <g transform="translate(300,160)">
+            <text font-weight="bold">
+                Order: {tooltipPt.order}
+            </text>
+        </g>
+    {/if}
+    
+    
+    <style>
+        .axisTitle {
+            font-weight: bold;
+        }
+    
+    </style>
     </svg>
 </div>
+
